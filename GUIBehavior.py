de80550459182a805f044dd6ddcb68214197b227
unknown <klenczner.marton@gmail.com>
2024-11-12 20:20:18 +0100

    on slider release, slider is reset. While holding slider, command is sent in user defined intervals

--- GUIBehavior.py
+++ GUIBehavior.py
@@ -1,12 +1,13 @@
 from typing import Optional
 
-from PySide6.QtGui import QPalette, QColor
+from PySide6.QtGui import QPalette, QColor, QRegularExpressionValidator
 from PySide6.QtWidgets import *
 from PySide6.QtCore import Qt, QCoreApplication, QObject, QEvent
 import PySide6
 import numpy as np
 from motorDriver import MotorDriver, INT16_MIN, INT16_MAX, UINT16_MIN, UINT16_MAX
-
+import threading
+import time
 
 
 class SliderRow():
@@ -15,7 +16,7 @@ class SliderRow():
     def __init__(self, parent, name, sliderMin=INT16_MIN, sliderMax=INT16_MAX, sliderStart=0, displayMin=-1.0,
                  displayMax=1.0):
 
-        self.parent = parent
+        self.parent: SliderBox = parent
         self.name = QLabel(parent)
         self.name.setText(name)
         self.name.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Fixed))
@@ -28,13 +29,20 @@ class SliderRow():
         self.slider.setSliderPosition(sliderStart)
         # pass scrolling event to parent to avoid changing value by scrolling
         self.slider.wheelEvent = lambda *args, **kwargs: self.parent.wheelEvent(*args, **kwargs)
+        self.sliderStart = sliderStart
 
         self.slider.valueChanged.connect(self.sliderValueChanged)
+        self.slider.sliderPressed.connect(self.sliderPressed)
+        self.slider.sliderReleased.connect(self.sliderReleased)
 
         self.displayMin = displayMin
         self.displayMax = displayMax
         self.valueLabel = QLabel(parent)
         self.displaySliderValue(sliderStart)
+
+        self.driverThread = None
+        self.released = True
+        self.sleep = 0.2
 
         self.widgets = [
             self.name,
@@ -61,7 +69,35 @@ class SliderRow():
 
     def sliderValueChanged(self, value):
         self.displaySliderValue(value)
-        self.parent.drive()
+        if not self.parent.checkBoxAbsRel.isChecked():
+            self.parent.drive()
+
+    def sliderPressed(self):
+        if self.parent.checkBoxAbsRel.isChecked():
+
+            self.driverThread = threading.Thread(target=self.sliderPressedLoop)
+            self.released = False
+            self.driverThread.start()
+
+    def sliderPressedLoop(self):
+        while not self.released:
+            self.parent.drive()
+            time.sleep(float(self.parent.waitBetweenSignals.text()))
+
+    def sliderReleased(self):
+        self.released = True
+        if self.driverThread is not None:
+            self.driverThread.join()
+
+        if self.parent.checkBoxAbsRel.isChecked():
+            self.resetSlider()
+        print("finished")
+
+    def resetSlider(self):
+        self.slider.blockSignals(True)
+        self.slider.setSliderPosition(self.sliderStart)
+        self.displaySliderValue(self.sliderStart)
+        self.slider.blockSignals(False)
 
     def addToGrid(self, grid: QGridLayout, startRow: int, startCol: int):
 
@@ -87,9 +123,17 @@ class SliderBox(QWidget):
         self.mainLayout = QGridLayout()
         self.mainLayout.setObjectName(str(self) + "mainLayout")
 
+        self.waitBetweenSignals = QLineEdit()
+        self.waitBetweenSignals.setValidator(QRegularExpressionValidator("[+]?([0-9]*[.])?[0-9]+"))
+        self.waitBetweenSignals.setMaximumWidth(80)
+        self.waitBetweenSignals.setText("0.1")
+        self.waitBetweenSignals.setEnabled(False)
+        self.mainLayout.addWidget(self.waitBetweenSignals, 2, 5, 1, 1)
+
         self.checkBoxAbsRel = QCheckBox(self)
         self.checkBoxAbsRel.setObjectName(str(self) + "checkBoxAbsRel")
         self.checkBoxAbsRel.setText("Relative")
+        self.checkBoxAbsRel.checkStateChanged.connect(self.checkBoxChanged)
         self.mainLayout.addWidget(self.checkBoxAbsRel, 2, 4, 1, 1)
 
         self.buttonParse = QPushButton(self)
@@ -121,6 +165,9 @@ class SliderBox(QWidget):
         for index, i in enumerate(self.sliders):
             i.addToGrid(self.mainLayout, 3 + index, 0)
 
+        self.spacer = QSpacerItem(20, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        self.mainLayout.addItem(self.spacer, 0, self.mainLayout.columnCount(), 1, 1)
+
     def drive(self):
         movementIDs, movementRatioModifiers = self.parseMovements()
         self.motorDriver.driveMotors(movementIDs, movementRatioModifiers,
@@ -128,6 +175,12 @@ class SliderBox(QWidget):
                                      self.sliders[1].slider.value(),
                                      self.sliders[2].slider.value(),
                                      self.getOptions())
+
+    def checkBoxChanged(self):
+        self.waitBetweenSignals.setEnabled(self.checkBoxAbsRel.isChecked())
+
+        for i in self.sliders:
+            i.resetSlider()
 
     def getOptions(self):
         options = 0
@@ -173,7 +226,8 @@ class SliderBox(QWidget):
         text = {
             "title": self.titleLine.text(),
             "movements": self.movementInputTextBox.toPlainText(),
-            "absRel": str(self.checkBoxAbsRel.isChecked())
+            "absRel": str(self.checkBoxAbsRel.isChecked()),
+            "waitTime": self.waitBetweenSignals.text()
         }
 
         return str(text)
@@ -186,3 +240,4 @@ class SliderBox(QWidget):
         self.titleLine.setText(d["title"])
         self.movementInputTextBox.setPlainText(d["movements"])
         self.checkBoxAbsRel.setChecked(d["absRel"] == "True")
+        self.waitBetweenSignals.setText(d["waitTime"])
