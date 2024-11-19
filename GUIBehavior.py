c3c5b4929855cbc01d2491c67b1746b25c2e3077
klenc <klenczner.marton@gmail.com>
2024-11-19 16:12:11 +0100

    change waitBetweenSignals to 0.05 s

--- GUIBehavior.py
+++ GUIBehavior.py
@@ -126,7 +126,7 @@ class SliderBox(QWidget):
         self.waitBetweenSignals = QLineEdit()
         self.waitBetweenSignals.setValidator(QRegularExpressionValidator("[+]?([0-9]*[.])?[0-9]+"))
         self.waitBetweenSignals.setMaximumWidth(80)
-        self.waitBetweenSignals.setText("0.06") # 0.06 worked better
+        self.waitBetweenSignals.setText("0.05")  # 0.05 worked better
         self.waitBetweenSignals.setEnabled(False)
         self.mainLayout.addWidget(self.waitBetweenSignals, 2, 5, 1, 1)
 
